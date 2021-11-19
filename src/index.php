2c098afdfe1d66d0d8f012efc74141885966264a
Leon Babic <leon@leonbabic.ch>
2021-11-19 19:09:07 +0100

    Initial commit

---
+++ src/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Example</title>
+</head>
+
+<body>
+    <div>
+        <h1>Holy shit, this is running!</h1>
+        <p>Look at this! This webpage is actually running. For testing purposes, a real time clock made with PHP (UTC):
+        <?php
+        date_default_timezone_set('UTC');
+        echo $timestamp = date('H:i:s');
+        ?>
+        </p>
+    </div>
+</body>
+
+</html>
